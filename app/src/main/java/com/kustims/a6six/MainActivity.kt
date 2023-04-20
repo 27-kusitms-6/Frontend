0b4c390b1fb529d84a841e9c688a0ea43d058816
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-04-20 18:52:14 +0900

    add/ui-lifeCycle 추가

--- app/src/main/java/com/kustims/a6six/MainActivity.kt
+++ app/src/main/java/com/kustims/a6six/MainActivity.kt
@@ -3,15 +3,40 @@ package com.kustims.a6six
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import com.kustims.a6six.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
+        initObserver()
+    }
+
+    //life cycle
+    fun initObserver()
+    {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+            }
+        }
     }
 
 }
