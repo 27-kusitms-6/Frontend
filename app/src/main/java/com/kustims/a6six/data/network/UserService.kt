7c114b4d1e5cdea324eaa9de3a5494b86e680dc7
MinseoShindor <tlsalstj01@duksung.ac.kr>
2023-05-23 22:19:03 +0900

    "add: MyPageService API"

--- app/src/main/java/com/kustims/a6six/data/network/UserService.kt
+++ app/src/main/java/com/kustims/a6six/data/network/UserService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
-interface UserApi {
+interface UserService {
 
     @GET("user/regenerateToken")
     suspend fun regenToken(): RegenTokenResponse
